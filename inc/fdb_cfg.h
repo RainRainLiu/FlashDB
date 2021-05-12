d657ceb4f3d4bf98511e003cbddd9c87bc286744
lynn <398958896@qq.com>
2021-05-12 16:44:25 +0800

    fal独立

--- inc/fdb_cfg.h
+++ inc/fdb_cfg.h
@@ -11,25 +11,35 @@
 
 #ifndef _FDB_CFG_H_
 #define _FDB_CFG_H_
-
+#include "b_config.h"
 /* using KVDB feature */
+#if _FDB_USING_KVDB
 #define FDB_USING_KVDB
+#endif
 
 #ifdef FDB_USING_KVDB
-/* Auto update KV to latest default when current KVDB version number is changed. @see fdb_kvdb.ver_num */
+/* Auto update KV to latest default when current KVDB version number is changed. @see
+ * fdb_kvdb.ver_num */
+#if _FDB_KV_AUTO_UPDATE
+#define FDB_KV_AUTO_UPDATE
+#endif
 /* #define FDB_KV_AUTO_UPDATE */
 #endif
 
 /* using TSDB (Time series database) feature */
+#if _FDB_USING_TSDB
 #define FDB_USING_TSDB
+#endif
 
 /* Using FAL storage mode */
+#if _FDB_USING_FAL_MODE
 #define FDB_USING_FAL_MODE
+#endif
 
 #ifdef FDB_USING_FAL_MODE
 /* the flash write granularity, unit: bit
  * only support 1(nor flash)/ 8(stm32f2/f4)/ 32(stm32f1) */
-#define FDB_WRITE_GRAN                /* @note you must define it for a value */
+#define FDB_WRITE_GRAN _FDB_WRITE_GRAN /* @note you must define it for a value */
 #endif
 
 /* Using file storage mode by LIBC file API, like fopen/fread/fwrte/fclose */
@@ -39,12 +49,13 @@
 /* #define FDB_USING_FILE_POSIX_MODE */
 
 /* MCU Endian Configuration, default is Little Endian Order. */
-/* #define FDB_BIG_ENDIAN */ 
+/* #define FDB_BIG_ENDIAN */
 
 /* log print macro. default EF_PRINT macro is printf() */
 /* #define FDB_PRINT(...)              my_printf(__VA_ARGS__) */
 
 /* print debug information */
+#if _FDB_DEBUG_ENABLE
 #define FDB_DEBUG_ENABLE
-
+#endif
 #endif /* _FDB_CFG_H_ */
